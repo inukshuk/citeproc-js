dad568367157604de2ec845ec14fd9e1e7983505
Sylvester Keil <sylvester@keil.or.at>
2011-08-03 16:35:55 +0200

    do not require rubygems

--- lib/citeproc/js.rb
+++ lib/citeproc/js.rb
@@ -1,13 +1,10 @@
 
 ENV['EXECJS_RUNTIME'] = 'RubyRhino'
 
-require 'rubygems'
-
 require 'ruby-debug'
 Debugger.start
 
 require 'citeproc'
 
 require 'citeproc/js/version'
-require 'citeproc/js/proxy'
 require 'citeproc/js/engine'
